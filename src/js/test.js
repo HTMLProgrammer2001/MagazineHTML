d69efc83a4dbc8b193c08cf65cea975ad4711409
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-03-19 21:34:29 +0200

    Add eslint

--- src/js/test.js
+++ src/js/test.js
@@ -1,38 +1,38 @@
 import React from 'react';
 
 class Elem extends React.Component{
-    constructor(props){
-        super(props);
+	constructor(props){
+		super(props);
 
-        this.state = {
-            count: 1
-        };
+		this.state = {
+			count: 1
+		};
 
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+	}
 
-    render() {
-        return (
-            <div>
-                <span onClick={this.decrement}>-</span>
-                <span>{this.state.count}</span>
-                <span onClick={this.increment}>+</span>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<span onClick={this.decrement}>-</span>
+				<span>{this.state.count}</span>
+				<span onClick={this.increment}>+</span>
+			</div>
+		);
+	}
 
-    increment(){
-        this.setState((prev) => ({
-            count: prev.count + 1
-        }));
-    }
+	increment(){
+		this.setState((prev) => ({
+			count: prev.count + 1
+		}));
+	}
 
-    decrement(){
-        this.setState((prev) => ({
-            count: prev.count - 1
-        }));
-    }
+	decrement(){
+		this.setState((prev) => ({
+			count: prev.count - 1
+		}));
+	}
 }
 
 export default Elem;
