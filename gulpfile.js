c350a98a3241acc4b6829d709c48b50d6308ab01
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-04-01 02:44:01 +0300

    Header ready

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
+const pugCompiler = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
-const lessCompiler = require('gulp-less');
 const imagemin = require('gulp-imagemin');
 const through = require('through2');
 const del = require('del');
+const rename = require('gulp-rename');
 const sourceMap = require('gulp-sourcemaps');
 const eslint = require('gulp-eslint');
+
 const browserSync = require('browser-sync').create();
 
 const webpackStream = require('webpack-stream');
@@ -18,44 +20,21 @@ const webpack = require('webpack');
 
 sass.compiler = require('node-sass');
 
-function css(){
-	return gulp.src('./src/css/*.css', {
-		allowEmpty: true
-	})
-		.pipe(concat('bundle.css'))
-		.pipe(sourceMap.init())
-		.pipe(autoprefixer())
-		.pipe(cleanCSS())
-		.pipe(sourceMap.write())
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.stream());
-}
-
 function scss(){
-	return gulp.src('./src/scss/main.scss', {
+	return gulp.src('./src/scss/*/*.scss', {
 		allowEmpty: true
 	})
 		.pipe(sourceMap.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('bundle.css'))
 		.pipe(autoprefixer())
 		.pipe(cleanCSS())
 		.pipe(sourceMap.write())
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.stream());
-}
-
-function less(){
-	return gulp.src('./src/less/main.less', {
-		allowEmpty: true
-	})
-		.pipe(sourceMap.init())
-		.pipe(lessCompiler())
-		.pipe(concat('bundle.css'))
-		.pipe(autoprefixer())
-		.pipe(cleanCSS())
-		.pipe(sourceMap.write())
-		.pipe(gulp.dest('./dist/css'))
+		.pipe(rename((path) => ({
+			basename: path.basename,
+			dirname: './',
+			extname: '.css'
+		})))
+		.pipe(gulp.dest('./dist/css/'))
 		.pipe(browserSync.stream());
 }
 
@@ -91,8 +70,16 @@ function image() {
 		.pipe(browserSync.stream());
 }
 
-function html() {
-	return gulp.src('./src/pages/*.html')
+function pug() {
+	return gulp.src('./src/pug/*/*.pug', {
+		allowEmpty: true
+	})
+		.pipe(pugCompiler())
+		.pipe(rename((path) => ({
+			basename: path.basename,
+			dirname: './',
+			extname: '.html'
+		})))
 		.pipe(gulp.dest('./dist/'))
 		.pipe(browserSync.stream());
 }
@@ -108,23 +95,20 @@ function watch(){
 		}
 	});
 
-	gulp.watch('./src/pages/**/*.html', html).on('change', browserSync.reload);
+	gulp.watch('./src/pug/**/*.pug', pug).on('change', browserSync.reload);
 	gulp.watch('./src/scss/**/*.scss', scss);
-	gulp.watch('./src/less/**/*.less', less);
-	gulp.watch('./src/css/**/*.css', css);
+	gulp.watch('./src/image/**/*.*', image);
 	gulp.watch('./src/js/**/*.js', js('development'));
 }
 
 function build(){
 	return gulp.series(clean, gulp.parallel(
-		html, css, scss, less, js('production'), image
+		pug, scss, js('production'), image
 	));
 }
 
-gulp.task('css', css);
 gulp.task('scss', scss);
-gulp.task('less', less);
-gulp.task('html', html);
+gulp.task('html', pug);
 gulp.task('js', js('production'));
 gulp.task('image', image);
 gulp.task('watch', watch);
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-import Counter from './test.js';
-
-ReactDOM.render(<Counter/>, document.getElementById('root'));
