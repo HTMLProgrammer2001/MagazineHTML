7ee10e6362e16004fdef932abb6795bb67148ace
HTMLProgrammer2001 <cssuperpy@gmail.com>
2020-04-11 19:55:06 +0300

    Add categories page and adaptive of single, main, checkout

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import autosize from 'autosize';
 import $ from 'jquery';
 
+//Dropdown
 $('.dropdown__elem').on('click', function(){
+	//Find dropdown elem
 	let dropdown = this.closest('.dropdown');
 
+	//Toggle state
 	if(!$(dropdown).hasClass('active')){
 		$('.dropdown').removeClass('active');
 		$(dropdown).addClass('active');
@@ -13,12 +16,14 @@ $('.dropdown__elem').on('click', function(){
 	}
 });
 
+//Burger
 $('.burger').on('click', function(){
 	$(this).toggleClass('active');
 
 	$('.header__links').toggleClass('active');
 });
 
+//Change account create status
 $('#create').on('change', function(){
 	if(this.checked) {
 		$('#createForm').removeClass('hidden');
@@ -28,11 +33,59 @@ $('#create').on('change', function(){
 	}
 });
 
+//Change active payment
 $('[name="payment"]').on('click', function(){
 	$('.payment__content').addClass('hidden');
 
 	$(this).closest('.payment__type').find('.payment__content')
 		.removeClass('hidden');
+});
+
+$('.goods__categories-title').on('click', function () {
+	$(this).parent().toggleClass('active');
+});
+
+
+//Range
+$('.goods__price-point').on('dragstart', () => false);
+
+$('.goods__price-point').on('mousedown', function (event) {
+	let elem = event.target,
+		indicator = document.querySelector('.goods__price-indicator'),
+		isRight = elem.classList.contains('right'),
+		leftRect = document.querySelector('.goods__price-point.left')
+			.getBoundingClientRect(),
+		rightRect = document.querySelector('.goods__price-point.right')
+			.getBoundingClientRect(),
+		parentRect = elem.parentElement.getBoundingClientRect();
+
+	function handler(event) {
+		if(isRight && (event.clientX > parentRect.right
+			|| event.clientX - leftRect.right <= 100 )){
+			return;
+		}
+
+		if(!isRight && (event.clientX <= parentRect.left
+			|| event.clientX - rightRect.left >= -100 )){
+			return;
+		}
+
+		elem.style.left = `${event.clientX - parentRect.left}px`;
+
+		if(isRight){
+			indicator.style.right = parentRect.width - parseInt(elem
+				.style.left) + 'px';
+		}
+		else{
+			indicator.style.left = elem.style.left;
+		}
+	}
+
+	$('body').on('mousemove', handler);
+
+	$('body').on('mouseup', function () {
+		$('body').off('mousemove', handler);
+	});
 });
 
 autosize(document.getElementById('message'));
